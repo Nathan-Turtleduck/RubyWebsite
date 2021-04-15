a2b34bb1380652b6231bf0cd701a712c59769fd5
Nathan Maroko <69446470+Nathan-Turtleduck@users.noreply.github.com>
2021-04-15 14:08:38 -0600

    Added Item.rb
    Defines an item class that holds a name,quantity, and price

---
+++ Item.rb
@@ -0,0 +1,54 @@
+#Author: Nathan Maroko
+#Date: 4/15/2021
+#Desc: Defines an Item object that stores a name,price, and quantity
+class Item
+  @@numItems = 0 #class variable - can be accessed from all objects
+
+  #Constructor method (uses initialize)
+  def initialize(name, price, quantity)
+    @itemName = name
+    @itemPrice = price
+    @itemQuant = quantity
+    @@numItems += 1
+  end
+
+  #returns the total price of the item(s) as a string
+  def getPrice()
+    total = @itemPrice * @itemQuant
+    totalString = "%0.2f" % [total]
+  end
+
+  #returns the numItems class variable
+  def getItems()
+    @@numItems
+  end
+
+  def getQuantity
+    @itemQuant
+  end
+
+  def setQuantity(quantity)
+    @itemQuant = quantity
+  end
+
+  def getPrice()
+    @itemPrice
+  end
+
+  def setPrice(price)
+    @itemPrice = price
+  end
+
+  def getName()
+    @itemName
+  end
+
+  def setName(name)
+    @itemName = name
+  end
+
+  def toString()
+    retString = "#{@itemQuant} #{@itemName}(s) at $#{@itemPrice}"
+  end  
+
+end
